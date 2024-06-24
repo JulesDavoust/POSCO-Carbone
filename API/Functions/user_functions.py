0d07272b755c97e63a4adb1f3049e134f88de13f
Jules <jules.davoust@yahoo.fr>
2024-06-24 17:50:07 +0200

    update

--- API/Functions/user_functions.py
+++ API/Functions/user_functions.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-import Models.test_model as User
+import Models.models as models
 from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
 import jwt
@@ -10,7 +10,7 @@ def signin(data, app):
     if not data or not data.get('email') or not data.get('password'):
         return jsonify({'message': 'Email and password are required!'}), 400
 
-    user = User.query.filter_by(email=data['email']).first()
+    user = models.Utilisateur_EFREI.query.filter_by(email=data['email']).first()
 
     if not user:
         return jsonify({'message': 'User Not found.'}), 404
@@ -46,12 +46,12 @@ def register(data, db):
         # 'isAdmin': False
     }
 
-    existing_user = User.query.filter_by(email=data['email']).first()
+    existing_user = models.Utilisateur_EFREI.query.filter_by(email=data['email']).first()
     if existing_user:
         return jsonify({'message': 'Email already taken'}), 500
 
     try:
-        user = User(**new_user)
+        user = models.Utilisateur_EFREI(**new_user)
         db.session.add(user)
         db.session.commit()
         return jsonify({'message': 'Registered'}), 200
